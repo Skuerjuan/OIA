8ae08fd1c084d09c80312384a2f4270f601050ac
Skuerjuan <maierowiczfelipe@gmail.com>
2024-05-23 10:55:12 -0300

    backup

--- OIA Juez/Desinfectando los Archivos/antivirus.cpp
+++ OIA Juez/Desinfectando los Archivos/antivirus.cpp
@@ -1,15 +1,24 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<fstream>
 using namespace std;
 
 int N;
 string text;
 char lett1;
 char lett2;
+string line;
 
 int main(){
-    cin >> N >> lett1 >> lett2;
-    cin >> text;
-    for(int i = 0; i < text.length(); ++i){
+    ifstream originalText ("antivirus.in");
+
+    if(originalText.is_open()){
+        while(getline(originalText, line)){
+            cout << line << endl;
+        }
+    }
+}
+/*     for(int i = 0; i < text.length(); ++i){
         if(text[i] == lett1 && text[i+1] == lett2 && text[i+2] == lett1){
                 text.erase(text.begin() + i+1);
                 i++;
@@ -17,4 +26,4 @@ int main(){
     }
     cout << text.length() << endl;
     cout << text;
-}
+} */
